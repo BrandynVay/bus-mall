ba524c01b4f7f594f11cde53adcc4cdc773f01b8
KlNGU <brandynvay@yahoo.com>
2019-03-03 17:30:19 -0800

    still need to fix gif

--- js/app.js
+++ js/app.js
@@ -23,20 +23,20 @@ var images = [
 
 var allImages = [];
 var busMall = document.getElementById('images');
-var justViewed = [];
+var viewedImage = [];
 var pics = [document.getElementById('left'), document.getElementById('center'), document.getElementById('right')];
-var numberOfClicks = document.getElementById('tally');
+var numberOfClicks = document.getElementById('number-of-clicks');
 var totalClicks = 0;
 
-function Product(name) {
+function BusMall(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = `img/${name}.jpg`;
   this.votes = 0;
   this.views = 0;
   allImages.push(this);
 }
 for(var i = 0; i < images.length; i++) {
-  new Product(images[i]);
+  new BusMall(images[i]);
 }
 
 function randomImage() {
@@ -46,39 +46,38 @@ function randomImage() {
 function displayImages() {
   var currentImage = [];
   currentImage[0] = randomImage();
-  while (justViewed.indexOf(currentImage[0]) !== -1) {
+  while (viewedImage.indexOf(currentImage[0]) !== -1) {
     currentImage[0] = randomImage();
   }
   currentImage[1] = randomImage();
-  while(currentImage[0] === currentImage[1] || justViewed.indexOf(currentImage[1]) !== -1) {
+  while(currentImage[0] === currentImage[1] || viewedImage.indexOf(currentImage[1]) !== -1) {
     currentImage[1] = randomImage();
   }
   currentImage[2] = randomImage();
-  while(currentImage[0] === currentImage[2] || currentImage[1] === currentImage[2] || justViewed.indexOf(currentImage[2]) !== -1) {
+  while(currentImage[0] === currentImage[2] || currentImage[1] === currentImage[2] || viewedImage.indexOf(currentImage[2]) !== -1) {
     currentImage[2] = randomImage();
   }
   for(var i = 0; i < 3; i++) {
     pics[i].src = allImages[currentImage[i]].path;
     pics[i].id = allImages[currentImage[i]].name;
     allImages[currentImage[i]].views += 1;
-    justViewed[i] = currentImage[i];
+    viewedImage[i] = currentImage[i];
   }
 }
 
 function handleClick(event) {
   console.log(totalClicks, 'total clicks');
   if(totalClicks >= 24) {
-    images.removeEventListener('click', handleClick);
     clicks();
   }
   if (event.target.id === 'images') {
-    return alert('Nope, you need to click on an image.');
+    return alert('Please click on a image!');
   }
   totalClicks += 1;
   for(var i = 0; i < images.length; i++) {
     if(event.target.id === allImages[i].name) {
       allImages[i].votes += 1;
-      console.log(event.target.id + ' has ' + allImages[i].votes + ' votes in ' + allImages[i].views + ' views');
+      console.log(`${event.target.id} has ${allImages[i].votes} votes and ${allImages[i].views} views`);
     }
   }
   displayImages();
@@ -87,7 +86,7 @@ function handleClick(event) {
 function clicks() {
   for(var i = 0; i < allImages.length; i++) {
     var liEl = document.createElement('li');
-    liEl.textContent = allImages[i].name + ' has ' + allImages[i].votes + ' votes in ' + allImages[i].views + ' views';
+    liEl.textContent = `${allImages[i].name} has ${allImages[i].votes} votes and ${allImages[i].views} views`;
     numberOfClicks.appendChild(liEl);
   }
 }
